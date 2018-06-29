0659382a0944c5b7f4963f3b5e0bd9ae9904ef24
rpradipta <39665490+rpradipta@users.noreply.github.com>
2018-06-29 21:28:47 +0700

    ganti head admin
    ganti jadi admin

--- application/views/include/head_admin.php
+++ application/views/include/head_admin.php
@@ -30,7 +30,7 @@
         <div class="row">
             <div class="col-sm-6">
                 <div class="logo">
-                    <h1><a href="<?php echo base_url() ?>Admin">Mobile<span> River</span>(Admin Panel)</a></h1>
+                    <h1><a href="<?php echo base_url() ?>Admin">FJB<span> UII</span>(Admin Panel)</a></h1>
                 </div>
             </div>
 
